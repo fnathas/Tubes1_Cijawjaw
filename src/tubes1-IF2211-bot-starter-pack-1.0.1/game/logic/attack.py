40cbbdc4cb164903320dbc7a857cca67ea65adfe
JasonFernandoo <j450n.fernando@gmail.com>
2024-03-06 18:55:25 +0700

    update attack

--- src/tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/attack.py
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/attack.py
@@ -14,57 +14,75 @@ class AttackLogic(BaseLogic):
         props = board_bot.properties
         current_position = board_bot.position
 
-        # Calculate distance to base
-        distance_to_base = abs(props.base.x - current_position.x) + abs(props.base.y - current_position.y)
-        
-        if props.diamonds == 5 or distance_to_base < 3:
-            # Move to base
+        # Check if there are nearby enemy bots
+        nearby_enemy_bots = [enemy_bot for enemy_bot in board.bots if enemy_bot != board_bot and self.is_nearby(current_position, enemy_bot.position)]
+
+        if nearby_enemy_bots:
+            # Attack the nearest enemy bot carrying diamonds
+            enemy_bots_with_diamonds = [enemy_bot for enemy_bot in nearby_enemy_bots if enemy_bot.properties.diamonds > 0]
+            if enemy_bots_with_diamonds:
+                nearest_enemy_bot = min(enemy_bots_with_diamonds, key=lambda bot: self.calculate_distance(current_position, bot.position))
+                self.goal_position = nearest_enemy_bot.position
+            else:
+                # Roam around and find the nearest diamond, but avoid diamonds if inventory is almost full
+                if props.diamonds < 4:
+                    self.goal_position = self.find_nearest_diamond(current_position, board.diamonds)
+                else:
+                    # Choose between picking up a diamond or returning to base
+                    nearest_diamond_position = self.find_nearest_diamond(current_position, board.diamonds)
+                    distance_to_base = self.calculate_distance(current_position, props.base)
+                    distance_to_nearest_diamond = self.calculate_distance(current_position, nearest_diamond_position)
+
+                    if distance_to_nearest_diamond < distance_to_base:
+                        self.goal_position = nearest_diamond_position
+                    else:
+                        self.goal_position = props.base
+        elif props.diamonds == 5:
+            # Move to base if carrying 5 diamonds
             self.goal_position = props.base
         else:
-            # Just roam around
-            self.goal_position = None
-        
-        if self.goal_position:
-            # We are aiming for a specific position, calculate delta
-            delta_x, delta_y = get_direction(
-                current_position.x,
-                current_position.y,
-                self.goal_position.x,
-                self.goal_position.y,
-            )
-        else:
-            # find the nearest diamond
-            min_distance_diamond = 1000000
-            nearest_diamond = None
-            
-            # find the nearest diamond
-            for diamond in board.diamonds:
-                distance = abs(diamond.position.x - current_position.x) + abs(diamond.position.y - current_position.y)
-                if distance < min_distance_diamond:
-                    min_distance_diamond = distance
-                    nearest_diamond = diamond
-            
-            # find the nearest enemy bot
-            min_distance_bot = 1000000
-            nearest_bot = None
-            
-            for enemy_bot in board.bots:
-                if enemy_bot != board_bot:  # Avoid detecting itself
-                    distance = abs(enemy_bot.position.x - current_position.x) + abs(enemy_bot.position.y - current_position.y)
-                    if distance < min_distance_bot:
-                        min_distance_bot = distance
-                        nearest_bot = enemy_bot
-            
-            # Compare distances and set the appropriate goal
-            if min_distance_diamond < min_distance_bot and min_distance_diamond < distance_to_base:
-                self.goal_position = nearest_diamond.position
-            elif nearest_bot and min_distance_bot < distance_to_base:
-                self.goal_position = nearest_bot.position
+            # Roam around and find the nearest diamond, but avoid diamonds if inventory is almost full
+            if props.diamonds < 4:
+                self.goal_position = self.find_nearest_diamond(current_position, board.diamonds)
+            else:
+                # Choose between picking up a diamond or returning to base
+                nearest_diamond_position = self.find_nearest_diamond(current_position, board.diamonds)
+                distance_to_base = self.calculate_distance(current_position, props.base)
+                distance_to_nearest_diamond = self.calculate_distance(current_position, nearest_diamond_position)
 
-            delta_x, delta_y = get_direction(
-                current_position.x,
-                current_position.y,
-                self.goal_position.x,
-                self.goal_position.y,
-            )
+                if distance_to_nearest_diamond < distance_to_base:
+                    self.goal_position = nearest_diamond_position
+                else:
+                    self.goal_position = props.base
+
+        # Calculate delta to reach the goal position
+        delta_x, delta_y = get_direction(
+            current_position.x,
+            current_position.y,
+            self.goal_position.x,
+            self.goal_position.y,
+        )
+
         return delta_x, delta_y
+
+    def is_nearby(self, position1: Position, position2: Position, distance_threshold: int = 1):
+        """Check if two positions are nearby within a given distance threshold."""
+        distance = abs(position1.x - position2.x) + abs(position1.y - position2.y)
+        return distance <= distance_threshold
+
+    def calculate_distance(self, position1: Position, position2: Position):
+        """Calculate the distance between two positions."""
+        return abs(position1.x - position2.x) + abs(position1.y - position2.y)
+
+    def find_nearest_diamond(self, current_position: Position, diamonds: list[GameObject]):
+        """Find the nearest diamond from the current position."""
+        min_distance = float('inf')
+        nearest_diamond_position = None
+
+        for diamond in diamonds:
+            distance = self.calculate_distance(current_position, diamond.position)
+            if distance < min_distance:
+                min_distance = distance
+                nearest_diamond_position = diamond.position
+
+        return nearest_diamond_position
