7649dffc8d30ebd590cb0fce08d3e2d10fcd2359
fnathas <farrel.saskoro@gmail.com>
2024-03-09 15:36:36 +0700

    refactor: fix bug on bot and finalize code

--- src/tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.bat
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.bat
@@ -1,5 +1,5 @@
 @echo off
-start cmd /c "python main.py --logic Attack --email=test@email.com --name=stima --password=123456 --team etimo"
-start cmd /c "python main.py --logic Random --email=test1@email.com --name=stima1 --password=123456 --team etimo"
-start cmd /c "python main.py --logic Random --email=test2@email.com --name=stima2 --password=123456 --team etimo"
-start cmd /c "python main.py --logic Random --email=test3@email.com --name=stima3 --password=123456 --team etimo"
+start cmd /c "python main.py --logic Ultimate --email=ulti@email.com --name=ultimate22 --password=123456 --team etimo"
+start cmd /c "python main.py --logic Attack --email=att@email.com --name=attack --password=123456 --team etimo"
+start cmd /c "python main.py --logic Greedy --email=fnth@email.com --name=Greedyzz --password=123456 --team etimo"
+start cmd /c "python main.py --logic Random --email=rand@email.com --name=Random --password=123456 --team etimo"
